bf8b46e75a82302dd697d84292171ca7be948082
BrunoCotaXavier <brunocota@hotmail.com.br>
2024-02-18 02:55:46 -0300

    implementando ver mais no blog

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -56,12 +56,10 @@
             @endrole
             <div class="mt-6 bg-gray shadow-sm rounded-lg">
                 @foreach ($blog as $blog_item)
-                <div class="bg-white p-6 flex space-x-2" style="margin-bottom: 20px; border-width: 1px;">
-                    <div style="">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600 -scale-x-100" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
-                        </svg>
-                    </div>
+                <div class="bg-white p-6 flex space-x-2" style="margin-bottom: 20px; border-width: 1px; padding-right: 55px;">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600 -scale-x-100" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
+                    </svg>
                     <div class="flex-1">
                         <div class="flex justify-between items-center">
                             <div>
