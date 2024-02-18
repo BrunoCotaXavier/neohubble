ad4cdb09677d4ba965af8f85a9b807f7f729a534
BrunoCotaXavier <brunocota@hotmail.com.br>
2024-02-18 17:02:53 -0300

    add tag google analytcs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,40 +1,54 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Neohubble') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <link rel="shortcut icon" href="{{ asset('images/logo.png') }}" type="image/x-icon">
+    <title>{{ config('app.name', 'Neohubble') }}</title>
 
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="shortcut icon" href="{{ asset('images/logo.png') }}" type="image/x-icon">
 
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-BPMSX0JLQX"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header>
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 ">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
 
-            <!-- Page Content -->
-            <mainx>
-                {{ $slot }}
+        gtag('config', 'G-BPMSX0JLQX');
+    </script>
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans antialiased">
+    <div class="min-h-screen bg-gray-100">
+        @include('layouts.navigation')
+
+        <!-- Page Heading -->
+        @if (isset($header))
+        <header>
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 ">
+                {{ $header }}
+            </div>
+        </header>
+        @endif
+
+        <!-- Page Content -->
+        <mainx>
+            {{ $slot }}
             </main>
-        </div>
-    </body>
+    </div>
+</body>
+
 </html>
